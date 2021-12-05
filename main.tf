e0a06653d9cb03d704c76bd3372af2ad9a74f659
AzureAD\DivyeshTandel <divyesh.tandel@visma.com>
2021-12-05 19:48:11 +0100

    add learn terraform project

---
+++ main.tf
@@ -0,0 +1,40 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+
+  backend "remote" {
+    organization = "LearnDevopsTF"
+
+    workspaces {
+      name = "DevopsTFCLIBased"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "westeurope"
+  tags = {
+    Environment = "Test"
+    Squad       = "SG-DivTFGroup"
+  }
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "westeurope"
+  resource_group_name = azurerm_resource_group.rg.name
+}
